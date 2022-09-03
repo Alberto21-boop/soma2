36c29160dafae4fb99d98d33b9ff80c4c6bbd136
Alberto <albertobarbosa0003@gmail.com>
2022-09-03 13:52:42 -0300

    Adicionar arquivos de projeto.

---
+++ soma2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace soma2;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Pergunta 1 de 1, int INDICE = 13, SOMA = 0, K = 0; Ao final do processamento, qual será o valor da variável SOMA?
+
+        int indice, soma, K;
+
+        indice = 13;
+        soma = 0;
+        K = 0;
+
+        Console.WriteLine("Entre com o numero inteiro zero por favor.");
+        Console.Write("Digite o zero = ");
+        K = int.Parse(Console.ReadLine());
+
+        while(K < indice)
+        {
+            soma = soma + K;
+            K++;
+        }
+        Console.WriteLine("O valor de soma é = " + soma);
+    }
+}
